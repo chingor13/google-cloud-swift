a52c1a7d50c9fea7e08a87908135871d04ddc746
Carlos O'Ryan <coryan@google.com>
2026-08-14 18:15:35 -0400

    cleanup: remove last references to `URLSession` (#428)
    We had one outdated integration / torture test, which is no longer
    needed.

--- Package.swift
+++ Package.swift
@@ -67,14 +67,13 @@ let package = Package(
   ] + generatedDependencies,
   targets: [
     .testTarget(
-      name: "IntegrationTests",
-      dependencies: [
-        .product(name: "GoogleCloudAuth", package: "auth")
-      ],
-    ),
-    .testTarget(
       name: "AllModules",
-      dependencies: [.product(name: "UserGuide", package: "guide")] + generatedModules,
+      dependencies: [
+        .product(name: "UserGuide", package: "guide"),
+        .product(name: "GoogleCloudAuth", package: "auth"),
+        .product(name: "GoogleCloudGax", package: "gax"),
+        .product(name: "GoogleCloudWkt", package: "wkt"),
+      ] + generatedModules,
     ),
     .testTarget(
       name: "Discovery",
